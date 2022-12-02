30c148066f0d68336b8c99292cb430e1852262f9
C6se <j41011@proton.me>
2022-12-02 12:05:32 +0000

    Initial Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.io.IOException;
 
 public class Main {
--- src/main/java/org/example/RPS.java
+++ src/main/java/org/example/RPS.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
